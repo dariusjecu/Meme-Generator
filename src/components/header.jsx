ae97047a75ce5eab4ed4692e86fa86a6d880c255
dariusjecu <dariusjecu@yahoo.com>
2022-12-28 16:49:46 +0000

    New

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Img from "../../public/img/logo.png"
 
 export default function Header(){
     return (
         <div className='header'>
             <div className='logo'>
-                <img src="../../public/img/logo.png" />
+                <img src={Img} />
                 <h1>Meme Generator</h1>
             </div>
             <h3></h3>  
